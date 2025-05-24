7debeb6d25bfde050c0a5c62141b7e2100b07a50
oquraan <oquraan52@gmail.com>
2025-05-24 23:13:28 +0300

    add new feature

--- js/index.js
+++ js/index.js
@@ -3,9 +3,19 @@ let obj = [],
 const CardContaier = document.getElementsByClassName("CardContaier")[0];
 let selectValue;
 const selectHouseValue = document.getElementById("selectHouse");
-
+const determinNum = document.getElementById("determinNum");
+let num = 16;
 selectValue = selectHouseValue.value;
+
+let InputName;
+let InputHouse;
+let InputBirthDate;
+let addButton;
+
 console.log(selectValue);
+determinNum.addEventListener("change", () => {
+  num = determinNum.value;
+});
 
 selectHouseValue.addEventListener("change", () => {
   selectValue = selectHouseValue.value;
@@ -13,7 +23,9 @@ selectHouseValue.addEventListener("change", () => {
   // fetchData();
   selectHouse();
   CardContaier.innerHTML = "";
-  displayData(obj2, selectValue);
+  displayData(obj2.slice(0, num), selectValue);
+  adjustCardLayout();
+  add();
 });
 
 function fetchData() {
@@ -25,8 +37,10 @@ function fetchData() {
       return response.json();
     })
     .then((data) => {
-      obj = data.slice(0, 16);
+      obj = data;
       displayData(data.slice(0, 16), selectValue);
+      adjustCardLayout();
+      add();
       // selectHouse();
     })
     .catch(() => {
@@ -35,10 +49,10 @@ function fetchData() {
       // const main1 = document.getElementsByClassName("CardContaier")[0];
       // main1.innerHTML = '<img class="imgclas" src="images/not-found.png">';
 
-      const main = document.getElementsByTagName("main")[0];
+      const main = document.getElementsByClassName("Error")[0];
       main.innerHTML = `
     
-          <img class="imgclass" src="images/not-found.png" alt="Error Image">
+          <img class="imgclass" src="./images/not-found.png" alt="Error Image">
       
       `;
       // main.style.backgroun = "url('images/not-found.png')";
@@ -53,7 +67,6 @@ function displayData(data, selectValue) {
     (element) => {
       // if (selectValue == "All houses") {
 
-      
       const card = document.createElement("div");
       card.className = "card";
       card.innerHTML = `         <img class="imgcard" src="${
@@ -155,6 +168,69 @@ function selectHouse() {
   //     obj2.push(element);
   //   });
 }
+function adjustCardLayout() {
+  const container = document.querySelector(".CardContaier");
+  const cards = container.querySelectorAll(".card");
+
+  if (cards.length === 2) {
+    container.style.justifyContent = "start";
+  } else {
+    container.style.justifyContent = "space-between";
+  }
+}
+
+function add() {
+  const card = document.createElement("div");
+  card.className = "card";
+  card.innerHTML = `         <img class="imgcard" src="images/not-found.png" />
+
+              
+              <div class="cardContant">
+
+    <input type="text" class="InputName Input" placeholder="Enter Name" />
+
+
+    <input type="text" class="InputHouse Input" placeholder="Enter House" />
+
+    <input type="date" class="InputBirthDate Input" placeholder="Enter BirthDate" />
+        <button id="addButton">Add Character</button>
+
+              </div>
+              </div>
+
+    `;
+  CardContaier.appendChild(card);
+
+  InputName = document.getElementsByClassName("InputName")[0];
+  InputHouse = document.getElementsByClassName("InputHouse")[0];
+  InputBirthDate = document.getElementsByClassName("InputName")[0];
+  addButton = document.getElementById("addButton");
+  addButton.addEventListener("click", () => {
+    console.log(InputBirthDate.value+"  "+ InputHouse.value+"  "+InputName.value);
+    obj.push({
+      name: InputName.value,
+      house: InputHouse.value,
+      dateOfBirth: InputBirthDate.value,
+    });
+  });
+}
+// }
+
+// // selectHouse();
+// document.addEventListener("DOMContentLoaded", () => {
+
+// });
 
 fetchData();
-// selectHouse();
+// const addButton = document.getElementById("addButton");
+
+// addButton.addEventListener("click", () => {
+//   const InputName = document.getElementsByClassName("InputName")[0];
+//   const InputHouse = document.getElementsByClassName("InputHouse")[0];
+//   const InputBirthDate = document.getElementsByClassName("InputName")[0];
+//   obj.push({
+//     name: InputName.value,
+//     house: InputHouse.value,
+//     dateOfBirth: InputBirthDate.value,
+//   });
+// });
